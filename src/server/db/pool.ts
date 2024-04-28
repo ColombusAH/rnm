1146dc992d024897beab72e237021eefd644ffee
Avner Hattab <avner.h@nanox.vision>
2024-04-28 10:08:52 +0300

    pool

--- src/server/db/pool.ts
+++ src/server/db/pool.ts
@@ -1,7 +1,10 @@
 import { Pool } from 'pg';
 
+const connectionString = 'postgresql://admin:admin123@localhost:5432/rickmortydb'
+const DATABASE_URL = process.env["DATABASE_URL"] || connectionString;
+
 const pool = new Pool({
-    connectionString: 'postgresql://admin:admin123@localhost:5432/rickmortydb'
+    connectionString: DATABASE_URL
 });
 
 export  {pool};
