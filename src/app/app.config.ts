222805a1c73eb3f8af179a46c6abc8f04ed09162
Avner Hattab <avner.h@nanox.vision>
2024-05-03 20:45:39 +0300

    pwa test

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom, InjectionToken, LOCALE_ID, Signal, signal } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, InjectionToken, LOCALE_ID, Signal, signal, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
@@ -11,6 +11,7 @@ import localeEn from '@angular/common/locales/en';
 import localHe from '@angular/common/locales/he';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
+import { provideServiceWorker } from '@angular/service-worker';
 
 
 export function HttpLoaderFactory(http: HttpClient) {
@@ -41,25 +42,23 @@ export function BodyDirection(translateService: TranslateService) {
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withInterceptors([tokenInterceptor])), provideAnimations(), importProvidersFrom(TranslateModule.forRoot(
+  providers: [provideRouter(routes), provideHttpClient(withInterceptors([tokenInterceptor])), provideAnimations(), importProvidersFrom(TranslateModule.forRoot({
+        defaultLanguage: 'he',
+        loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+        }
+    }), ToastModule), MessageService, { provide: DIRECTION_TOKEN, useFactory: BodyDirection, deps: [TranslateService] },
     {
-      defaultLanguage: 'he',
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    },
-  ),ToastModule),MessageService, { provide: DIRECTION_TOKEN, useFactory: BodyDirection, deps: [TranslateService] },
-
-  {
-    provide: LOCALE_ID,
-
-    useFactory: () => {
-      const lang = localStorage.getItem('lang') || 'he';
-      return lang === 'he' ? 'he-IL' : 'en-US';
-    }
-  }
-  ],
+        provide: LOCALE_ID,
+        useFactory: () => {
+            const lang = localStorage.getItem('lang') || 'he';
+            return lang === 'he' ? 'he-IL' : 'en-US';
+        }
+    }, provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })],
 
 };
